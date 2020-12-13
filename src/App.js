d00fb8610d038b869d2518b70cdc284c0ebccfa4
melodeenae5 <danaekirby5@gmail.com>
2020-12-13 14:52:24 -0600

    added resume link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Pdf from './resume/danae_kirby_resume.pdf';
 
 function App() {
-	return <div className='App'>Hello world!</div>;
+	return (
+		<div className='App'>
+			Hello world!
+			<a href={Pdf} target='_blank'>
+				Resume
+			</a>
+		</div>
+	);
 }
 
 export default App;
